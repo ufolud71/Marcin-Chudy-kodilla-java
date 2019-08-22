d6533c517c8fa66d252c7af069445f23f0c9db6d
ufolud71 <chudy.orfeusz@gmail.com>
2019-08-22 19:57:00 +0200

    calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator(10,5);
+
+        int result1 = calculator.add();
+
+        int result2 = calculator.substract();
+
+        if(result1 == 15) {
+            System.out.println("Adding method works!");
+        } else {
+            System.out.println("Adding method doesn,t work :(");
+        }
+
+        if (result2 == 5) {
+            System.out.println("Substracting method works!");
+        } else {
+            System.out.println("Substracting method doesn,t work :(");
+        }
+
     }
 }
