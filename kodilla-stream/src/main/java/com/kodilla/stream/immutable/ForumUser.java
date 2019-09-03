a1501ad7046d0258136851d226c50f72981ea36b
ufolud71 <chudy.orfeusz@gmail.com>
2019-09-03 17:40:19 +0200

    final

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,10 +1,10 @@
 package com.kodilla.stream.immutable;
 
 public final class ForumUser {
-    private String username;
-    private String realName;
+    private final String username;
+    private final String realName;
 
-    public ForumUser(String username, String realName) {
+    public ForumUser(final String username, final String realName) {
         this.username = username;
         this.realName = realName;
     }
