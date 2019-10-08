8026d097998219bd6afd531e6df0f576dacb37f0
ufolud71 <chudy.orfeusz@gmail.com>
2019-10-08 17:19:01 +0200

    MovieStore

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
@@ -0,0 +1,24 @@
+package com.kodilla.good.patterns.challenges;
+
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.*;
+
+public class MovieStoreMain {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String finalList = movieStore.getMovies().entrySet().stream()
+                .map(e -> e.getValue())
+                .flatMap(e -> e.stream())
+                .collect(Collectors.joining("!"));
+
+
+        System.out.println(finalList);
+
+    }
+}
