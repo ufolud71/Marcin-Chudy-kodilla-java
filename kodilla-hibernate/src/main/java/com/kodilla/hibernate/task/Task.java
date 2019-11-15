af5885a85280b93d3762a8f837512adda09c82a9
ufolud71 <chudy.orfeusz@gmail.com>
2019-11-15 21:54:49 +0100

    Invoice

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,5 +1,7 @@
 package com.kodilla.hibernate.task;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
@@ -12,6 +14,8 @@ public class Task {
     private String description;
     private Date created;
     private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
+    private TaskList tasklist;
 
     public Task() {
 
@@ -34,6 +38,7 @@ public class Task {
     public String getDescription() {
         return description;
     }
+
     @NotNull
     @Column (name= "CREATED")
     public Date getCreated() {
@@ -43,6 +48,26 @@ public class Task {
     @Column (name = "DURATION")
     public int getDuration() {
         return duration;
+    }
+
+    @OneToOne (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn (name = "TASKS_FINANCIAL_ID")
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
+
+    @ManyToOne
+    @JoinColumn (name = "TASKLIST_ID")
+    public TaskList getTaskList() {
+        return tasklist;
+    }
+
+    public void setTaskList(TaskList tasklist) {
+        this.tasklist = tasklist;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
     }
 
     public void setId(int id) {
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,66 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    private InvoiceDao invoiceDao;
+    @Autowired
+    private ProductDao productDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Item item1 = new Item(new BigDecimal(20), 5, new BigDecimal(100));
+        Item item2 = new Item(new BigDecimal(4), 3, new BigDecimal(12));
+        Item item3 = new Item(new BigDecimal(125), 2, new BigDecimal(250));
+
+        Product product = new Product("Shop1");
+        product.getItems().add(item1);
+        product.getItems().add(item2);
+        product.getItems().add(item3);
+
+        Invoice invoice = new Invoice("20031");
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        invoice.getItems().add(item3);
+
+
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+        item3.setInvoice(invoice);
+
+        item1.setProduct(product);
+
+
+        //When
+        invoiceDao.save(invoice);
+
+        int id = invoice.getId();
+
+
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+
+
+        //CleanUp
+        //invoiceDao.deleteById(id);
+
+    }
+}
+
+
+
